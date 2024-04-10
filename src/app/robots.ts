7518b1505d930930d8e743a55f5f083d16af3978
Orkun Karakus <aorkunkarakus@gmail.com>
2024-04-11 02:20:29 +0300

    sitemap added

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+	return {
+		rules: [
+			{
+				userAgent: '*'
+			}
+		],
+		sitemap: 'https://orkunkarakus.com/sitemap.xml',
+		host: 'https://orkunkarakus.com'
+	};
+}
